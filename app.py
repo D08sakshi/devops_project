a347a86efb1379947995ff1d65c7d5899fda2ae8
Sakshi <sakshigaitonde970@gmail.com>
2026-05-24 22:57:29 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task = request.form.get('task')
+    
+    if task:
+        tasks.append({
+            'title': task,
+            'done': False
+        })
+    
+    return redirect('/')
+
+@app.route('/complete/<int:task_id>')
+def complete_task(task_id):
+    tasks[task_id]['done'] = True
+    return redirect('/')
+
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    tasks.pop(task_id)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
